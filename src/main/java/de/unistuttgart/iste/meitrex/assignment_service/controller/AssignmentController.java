0c29938d9676e866b9cfd298a4d08ace684e3221
julian.lxs <juliangruber.su@gmail.com>
2024-08-19 14:50:17 +0200

    complete mutation and schema mapping in controller

--- src/main/java/de/unistuttgart/iste/meitrex/assignment_service/controller/AssignmentController.java
+++ src/main/java/de/unistuttgart/iste/meitrex/assignment_service/controller/AssignmentController.java
@@ -1,11 +1,11 @@
 package de.unistuttgart.iste.meitrex.assignment_service.controller;
 
-import de.unistuttgart.iste.meitrex.generated.dto.Assignment;
+import de.unistuttgart.iste.meitrex.common.user_handling.LoggedInUser;
+import de.unistuttgart.iste.meitrex.generated.dto.*;
 import de.unistuttgart.iste.meitrex.assignment_service.service.AssignmentService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.graphql.data.method.annotation.Argument;
-import org.springframework.graphql.data.method.annotation.QueryMapping;
+import org.springframework.graphql.data.method.annotation.*;
 import org.springframework.stereotype.Controller;
 
 import java.util.List;
@@ -18,9 +18,62 @@ public class AssignmentController {
 
     private final AssignmentService assignmentService;
 
+    /* Query Mappings */
+
     @QueryMapping
     public List<Assignment> findAssignmentsByAssessmentIds(@Argument List<UUID> assessmentIds) {
         return assignmentService.findAssignmentsByAssessmentIds(assessmentIds);
+    }
+
+    /* Mutation Mappings */
+
+    @MutationMapping(name = "_internal_noauth_createAssignment")
+    public Assignment createAssignment(@Argument final UUID courseId,
+                                       @Argument final UUID assessmentId,
+                                       @Argument final CreateAssignmentInput input) {
+        return assignmentService.createAssignment(courseId, assessmentId, input);
+    }
+
+    @MutationMapping
+    public AssignmentMutation mutateAssignment(@Argument final UUID assessmentId) {
+        return assignmentService.mutateAssignment(assessmentId);
+    }
+
+    @MutationMapping
+    public AssignmentCompletedFeedback logAssignmentCompleted(@Argument LogAssignmentCompletedInput input, @ContextValue final LoggedInUser currentUser) {
+        return assignmentService.logAssignmentCompleted(input, currentUser.getId());
+    }
+
+    /* Schema Mappings */
+
+    @SchemaMapping(typeName = "AssignmentMutation")
+    public Exercise createExercise(@Argument(name = "input") final CreateExerciseInput input, final AssignmentMutation assignmentMutation) {
+        return assignmentService.createExercise(assignmentMutation.getAssessmentId(), input);
+    }
+
+    @SchemaMapping(typeName = "AssignmentMutation")
+    public Exercise updateExercise(@Argument(name = "input") final UpdateExerciseInput input, final AssignmentMutation assignmentMutation) {
+        return assignmentService.updateExercise(assignmentMutation.getAssessmentId(), input);
+    }
+
+    @SchemaMapping(typeName = "AssignmentMutation")
+    public UUID deleteExercise(@Argument(name = "itemId") final UUID itemId, final AssignmentMutation assignmentMutation) {
+        return assignmentService.deleteExercise(assignmentMutation.getAssessmentId(), itemId);
+    }
+
+    @SchemaMapping(typeName = "AssignmentMutation")
+    public Subexercise createSubexercise(@Argument(name = "input") final CreateSubexerciseInput input, final AssignmentMutation assignmentMutation) {
+        return assignmentService.createSubexercise(assignmentMutation.getAssessmentId(), input);
+    }
+
+    @SchemaMapping(typeName = "AssignmentMutation")
+    public Subexercise updateSubexercise(@Argument(name = "input") final UpdateSubexerciseInput input, final AssignmentMutation assignmentMutation) {
+        return assignmentService.updateSubexercise(assignmentMutation.getAssessmentId(), input);
+    }
+
+    @SchemaMapping(typeName = "AssignmentMutation")
+    public UUID deleteSubexercise(@Argument(name = "itemId") final UUID itemId, final AssignmentMutation assignmentMutation) {
+        return assignmentService.deleteSubexercise(assignmentMutation.getAssessmentId(), itemId);
     }
 
 }
