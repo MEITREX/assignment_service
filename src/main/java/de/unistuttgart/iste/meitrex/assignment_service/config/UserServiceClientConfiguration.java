1f2cbc00571304a6a67abaac3d7484baa3cd5442
Mykhailo Zelia <mishazelea@gmail.com>
2025-05-03 12:20:31 +0200

    Add missing import

--- src/main/java/de/unistuttgart/iste/meitrex/assignment_service/config/UserServiceClientConfiguration.java
+++ src/main/java/de/unistuttgart/iste/meitrex/assignment_service/config/UserServiceClientConfiguration.java
@@ -1,5 +1,6 @@
 package de.unistuttgart.iste.meitrex.assignment_service.config;
 
+import de.unistuttgart.iste.meitrex.user_service.client.UserServiceClient;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
